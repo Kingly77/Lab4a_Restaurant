ac4b4d0a25ec77ae01f168d989d3ee8f92d806d4
kingly <shelbygj97@gmail.com>
2025-09-02 11:57:35 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,103 @@
+// COMSC-210 | Lab 4A | Shelby Jordan
+// IDE used: Clion
+
+#include <iostream>
+
+struct Restaurants {
+    std::string name;
+    int age = 0;
+    float rating = 0.0;
+    std::string type;
+    int number_of_items = 0;
+};
+
+void get_restaurant_name_input(Restaurants &restaurants) {
+    std::cout << "Enter the name of the restaurant: ";
+    try {
+
+        std::getline(std::cin, restaurants.name);
+        if (std::cin.fail()) throw std::exception();
+    }catch (std::exception &e) {
+        std::cin.ignore();
+        std::cout << "Error: Invalid input try again" << std::endl;
+        get_restaurant_name_input(restaurants);
+    }
+}
+
+
+void collect_restaurant_age_input(Restaurants &restaurants) {
+    std::cout << "Enter the age of the restaurant: ";
+    try {
+        std::cin >> restaurants.age;
+        if (std::cin.fail()) throw std::exception();
+    }catch (std::exception &e) {
+        std::cin.ignore();
+        std::cout << "Error: Invalid input try again" << std::endl;
+        collect_restaurant_age_input(restaurants);
+    }
+}
+
+void get_restaurant_rating(Restaurants &restaurants) {
+    std::cout << "Enter the rating of the restaurant: ";
+    try {
+        std::cin.ignore();
+        std::cin >>restaurants.rating;
+        if (std::cin.fail()) throw std::exception();
+    } catch (std::exception &e) {
+        std::cout << "Error: Invalid input try again" << std::endl;
+        get_restaurant_rating(restaurants);
+    }
+}
+
+void get_restaurant_type_input(Restaurants &restaurants) {
+    std::cout << "Enter the type of the restaurant: ";
+    try {
+        std::getline(std::cin,restaurants.type);
+        if (std::cin.fail()) throw std::exception();
+    } catch (std::exception &e) {
+        std::cin.ignore();
+        std::cout << "Error: Invalid input try again" << std::endl;
+        get_restaurant_type_input(restaurants);
+    }
+}
+
+void get_restaurant_item_count(Restaurants &restaurants) {
+    std::cout << "Enter the number of items in the restaurant: ";
+    try {
+        std::cin >> restaurants.number_of_items;
+        if (std::cin.fail()) throw std::exception();
+    } catch (std::exception &e) {
+        std::cin.ignore();
+        std::cout << "Error: Invalid input try again" << std::endl;
+        get_restaurant_item_count(restaurants);
+    }
+}
+
+[[nodiscard]]
+Restaurants makeRestaurants() {
+    Restaurants restaurants;
+
+    get_restaurant_name_input(restaurants);
+    collect_restaurant_age_input(restaurants);
+    get_restaurant_rating(restaurants);
+    get_restaurant_type_input(restaurants);
+    get_restaurant_item_count(restaurants);
+    return restaurants;
+}
+
+void displayRestaurant(const Restaurants &restaurant) {
+    std::cout << "Name: " << restaurant.name << std::endl;
+    std::cout << "Age: " << restaurant.age << std::endl;
+    std::cout << "Rating: " << restaurant.rating << std::endl;
+    std::cout << "Type: " << restaurant.type << std::endl;
+    std::cout << "Number of items: " << restaurant.number_of_items << std::endl;
+}
+
+int main() {
+    for (auto i = 0; i < 5; i++)
+        {
+        Restaurants newRestaurant = makeRestaurants();
+        displayRestaurant(newRestaurant);
+    }
+    return 0;
+}
